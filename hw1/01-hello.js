ccb202133fe540ecc629eebf13f642a53c5d1066
khose@pdx.edu <shivrajkhose4>
2024-01-28 03:30:39 -0800

    Homework_1

--- hw1/01-hello.js
+++ hw1/01-hello.js
@@ -1,5 +1,8 @@
-window.addEventListener('load', loadPage);
+window.addEventListener("load", loadPage);
 
 function loadPage(event) {
-  console.log('The page has now loaded.');
+  console.log("The page has now loaded.");
+  console.log(
+    "My name is Shivraj and I am currently pursuing my Master's in Computer Science starting from Fall 2023. I am really excited to learn web development. I am excited about creating responsive web pages. I like to play Chess and Cricket. Thank you!"
+  );
 }
